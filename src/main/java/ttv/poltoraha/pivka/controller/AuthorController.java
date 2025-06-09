68a19580b482e38a863f2d7cd057ae8122910cc5
PIVO-102-AniMoon <lpline15@mail.ru>
2025-06-09 21:47:07 +0800

    108

--- src/main/java/ttv/poltoraha/pivka/controller/AuthorController.java
+++ src/main/java/ttv/poltoraha/pivka/controller/AuthorController.java
@@ -6,6 +6,8 @@ import ttv.poltoraha.pivka.dao.request.AuthorDto;
 import ttv.poltoraha.pivka.entity.Author;
 import ttv.poltoraha.pivka.entity.Book;
 import ttv.poltoraha.pivka.service.AuthorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -16,19 +18,27 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AuthorController {
     private final AuthorService authorService;
+    private final Logger logger = LoggerFactory.getLogger(AuthorController.class);
 
     @PostMapping("/create")
     public void createAuthor(@RequestBody AuthorDto authorDto) {
+        logger.info("Received request to create author: {}", authorDto);
         authorService.create(authorDto);
+        logger.info("Successfully created author: {}", authorDto);
     }
 
     @PostMapping("/delete")
     public void deleteAuthorById(@RequestParam Integer id) {
+        logger.info("Received request to delete author with id: {}", id);
         authorService.delete(id);
+        logger.info("Successfully deleted author with id: {}", id);
     }
 
     @PostMapping("/add/books")
     public void addBooksToAuthor(@RequestParam Integer id, @RequestBody List<Book> books) {
+        logger.info("Received request to add books to author with id: {}", id);
+        logger.info("Books to add: {}", books);
         authorService.addBooks(id, books);
+        logger.info("Successfully added books to author with id: {}", id);
     }
 }
--- src/main/java/ttv/poltoraha/pivka/serviceImpl/AuthorServiceImpl.java
+++ src/main/java/ttv/poltoraha/pivka/serviceImpl/AuthorServiceImpl.java
@@ -13,6 +13,8 @@ import ttv.poltoraha.pivka.entity.Author;
 import ttv.poltoraha.pivka.entity.Book;
 import ttv.poltoraha.pivka.repository.AuthorRepository;
 import ttv.poltoraha.pivka.service.AuthorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -22,12 +24,15 @@ import java.util.List;
 @Transactional
 public class AuthorServiceImpl implements AuthorService {
     private final AuthorRepository authorRepository;
+    private final Logger logger = LoggerFactory.getLogger(AuthorServiceImpl.class);
 
     // todo как будто надо насрать всякими мапперами
     @Override
     public void create(AuthorDto authorDto) {
+        logger.info("Received request to create author: {}", authorDto);
         // Проверяем, существует ли автор с таким же именем
         if (authorRepository.existsByFullName(authorDto.getFullName())) {
+            logger.error("Author with name = {} already exists", authorDto.getFullName());
             throw new EntityExistsException(String.format("Author with name = %s already exists", authorDto.getFullName()));
         }
 
@@ -37,33 +42,44 @@ public class AuthorServiceImpl implements AuthorService {
         author.setAvgRating(authorDto.getAvgRating()); // Если avgRating не нужен, можно убрать эту строку
 
         // Сохраняем автора в репозитории
+        logger.info("Saving author to database: {}", author);
         authorRepository.save(author);
+        logger.info("Successfully created author: {}", author);
     }
 
     @Override
     public void delete(Integer id) {
+        logger.info("Received request to delete author with id: {}", id);
         authorRepository.deleteById(id);
+        logger.info("Successfully deleted author with id: {}", id);
     }
 
     @Override
     public void addBooks(Integer id, List<Book> books) {
+        logger.info("Received request to add books to author with id: {}", id);
         val author = getOrThrow(id);
 
+        logger.info("Adding books to author: {}", author);
         author.getBooks().addAll(books);
+        logger.info("Successfully added books to author with id: {}", id);
     }
 
     @Override
     public void addBook(Integer id, Book book) {
+        logger.info("Received request to add a book to author with id: {}", id);
         val author = getOrThrow(id);
 
+        logger.info("Adding book to author: {}", author);
         author.getBooks().add(book);
+        logger.info("Successfully added book to author with id: {}", id);
     }
 
     @Override
     public List<Author> getTopAuthorsByTag(String tag, int count) {
+        logger.info("Received request to get top authors by tag: {} with count: {}", tag, count);
         Pageable pageable = PageRequest.of(0, count);
         val authors = authorRepository.findTopAuthorsByTag(tag);
-
+        logger.info("Successfully retrieved top authors by tag: {}", tag);
         return authorRepository.findTopAuthorsByTag(tag, pageable);
     }
 
@@ -72,6 +88,7 @@ public class AuthorServiceImpl implements AuthorService {
         val author = optionalAuthor.orElse(null);
 
         if (author == null) {
+            logger.error("Author with id = {} not found", id);
             throw new RuntimeException("Author with id = " + id + " not found");
         }
 
