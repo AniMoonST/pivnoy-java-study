a77868ea6544390474b1b2d5beba3f1abf5c5194
Dmitriy <lpline16@gmail.com>
2025-06-13 22:23:47 +0800

    109

--- src/main/java/ttv/poltoraha/pivka/controller/AuthorController.java
+++ src/main/java/ttv/poltoraha/pivka/controller/AuthorController.java
@@ -8,6 +8,9 @@ import ttv.poltoraha.pivka.entity.Book;
 import ttv.poltoraha.pivka.service.AuthorService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+import io.micrometer.core.instrument.Counter;
 
 import java.util.List;
 
@@ -15,30 +18,53 @@ import java.util.List;
 // Или если у приложухи есть веб-морда, каждое действие пользователя - это http запросы
 @RestController
 @RequestMapping("/author")
-@RequiredArgsConstructor
 public class AuthorController {
     private final AuthorService authorService;
     private final Logger logger = LoggerFactory.getLogger(AuthorController.class);
+    private final MeterRegistry meterRegistry;
+
+    // Создаем счетчик и таймер
+    private final Counter requestCounter;
+    private final Timer dbTimer;
+
+    public AuthorController(AuthorService authorService, MeterRegistry meterRegistry) {
+        this.authorService = authorService;
+        this.meterRegistry = meterRegistry;
+        this.requestCounter = meterRegistry.counter("author_requests_count");
+        this.dbTimer = meterRegistry.timer("author_db_timer");
+    }
 
     @PostMapping("/create")
     public void createAuthor(@RequestBody AuthorDto authorDto) {
         logger.info("Received request to create author: {}", authorDto);
-        authorService.create(authorDto);
-        logger.info("Successfully created author: {}", authorDto);
+        requestCounter.increment(); // Увеличиваем счетчик запросов
+
+        dbTimer.record(() -> {
+            authorService.create(authorDto); // Измеряем время выполнения
+            logger.info("Successfully created author: {}", authorDto);
+        });
     }
 
     @PostMapping("/delete")
     public void deleteAuthorById(@RequestParam Integer id) {
         logger.info("Received request to delete author with id: {}", id);
-        authorService.delete(id);
-        logger.info("Successfully deleted author with id: {}", id);
+        requestCounter.increment(); // Увеличиваем счетчик запросов
+
+        dbTimer.record(() -> {
+            authorService.delete(id); // Измеряем время выполнения
+            logger.info("Successfully deleted author with id: {}", id);
+        });
     }
 
     @PostMapping("/add/books")
     public void addBooksToAuthor(@RequestParam Integer id, @RequestBody List<Book> books) {
         logger.info("Received request to add books to author with id: {}", id);
         logger.info("Books to add: {}", books);
-        authorService.addBooks(id, books);
-        logger.info("Successfully added books to author with id: {}", id);
+        requestCounter.increment(); // Увеличиваем счетчик запросов
+
+        dbTimer.record(() -> {
+            authorService.addBooks(id, books); // Измеряем время выполнения
+            logger.info("Successfully added books to author with id: {}", id);
+        });
     }
 }
