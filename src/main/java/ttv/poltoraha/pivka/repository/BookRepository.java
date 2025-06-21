14ba89ca7655ede26e9bbda24f02897070f7469a
Dmitriy <lpline16@gmail.com>
2025-06-21 08:56:50 +0800

    202

--- src/main/java/ttv/poltoraha/pivka/repository/BookRepository.java
+++ src/main/java/ttv/poltoraha/pivka/repository/BookRepository.java
@@ -2,6 +2,7 @@ package ttv.poltoraha.pivka.repository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ttv.poltoraha.pivka.entity.Book;
 
@@ -15,4 +16,10 @@ public interface BookRepository extends CrudRepository<Book, Integer> {
             "AND a.rating = (SELECT MAX(a2.rating) FROM authors a2 WHERE a2.last_name = :lastName)",
             nativeQuery = true)
     List<Book> findBooksByAuthorWithHighestRating(String lastName);
+
+    @Query("SELECT b FROM book b WHERE b.tags LIKE %:tag% ORDER BY b.rating DESC")
+    List<Book> findTop3BooksByTag(@Param("tag") String tag);
+
+    @Query("SELECT b FROM book b WHERE b.tags LIKE %:tag% ORDER BY b.rating DESC")
+    List<Book> findTop2BooksByTag(@Param("tag") String tag);
 }
